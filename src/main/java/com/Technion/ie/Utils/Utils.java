341122b6fbb12cb5e4cd730ac1c25559bbdf6104
Eilons <eilon16@gmail.com>
2014-11-29 23:32:31 +0200

    finish part a: read trees, parse rare words and write to file

--- src/main/java/com/Technion/ie/Utils/Utils.java
+++ src/main/java/com/Technion/ie/Utils/Utils.java
@@ -2,6 +2,11 @@ package com.Technion.ie.Utils;
 
 import java.util.List;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,9 +15,28 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Utils {
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.util.JSONPObject;
+import org.codehaus.jackson.node.ObjectNode;
+import org.codehaus.jackson.node.ArrayNode;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-	public static Map<String,Short> readQueries() throws IOException, URISyntaxException {
+/**
+ * @author Eilon
+ *
+ */
+
+public class Utils {
+	public static String RESULT_LINE_FORMAT = "%s\n";
+
+	public static Map<String,Short> readCountsFile() throws IOException, URISyntaxException {
 		String countsFile = "/cfgCounts.txt";
 		Map<String,Short> wordCountMap = new TreeMap<String, Short>();
 		InputStream wordCountFile = Utils.class.getResourceAsStream(countsFile);
@@ -37,6 +61,42 @@ public class Utils {
 		return wordCountMap;
 	}
 	
+	/**
+	 * @param wordCountMap
+	 * @throws IOException
+	 * @throws ParseException
+	 * Main func : read line by line from a file, each line is tree.
+	 * return each tree with _RARE_ symbol for rare words
+	 */
+	public static List<JSONArray> buildRareTrees (Map<String,Short> wordCountMap ) throws IOException, ParseException
+	{
+		List<String> treeLines = new ArrayList<String>();//contain all tree rows
+		List<JSONArray> parseTrees = new ArrayList<JSONArray>();
+		treeLines = Utils.readTreeFile("/parse_train.dat");
+		
+		for (String line : treeLines) 
+		//each line is tree to parse
+		{
+			JSONArray tree = new JSONArray();
+			tree = Utils.constructJsonTrees(line);
+			Utils.replaceRareWords(tree,wordCountMap);
+			parseTrees.add(tree);
+		}
+		return parseTrees;
+	}
+	
+	private static List<String> readTreeFile (String path) throws IOException, ParseException {
+		InputStream wordFile = Utils.class.getResourceAsStream(path);
+		List<String> allLines = Utils.readAllLines(wordFile);
+		ArrayList<String> treeList  = new ArrayList<String>();
+		for (String line : allLines) {
+				treeList.add(line);	
+			
+			
+		}
+		return treeList;
+	}
+	
 	private static List<String> readAllLines(InputStream inputFile) throws IOException {
 		List<String> countsList = new ArrayList<String>();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputFile));
@@ -46,4 +106,81 @@ public class Utils {
 		}
 		return countsList;
 	}
+
+
+
+	private static JSONArray constructJsonTrees(String line) throws JsonParseException, JsonMappingException, IOException, ParseException
+	{
+		JSONArray rootNode = null;
+		JSONParser parser = new JSONParser();
+		rootNode = (JSONArray) parser.parse(line);
+		return rootNode;
+	}
+	
+	private static void replaceRareWords (JSONArray rootNode , Map<String,Short> wordCountMap)
+	//Takes a nested list in loaded JSON format and a set of rare (word, tag) pairs 
+	//    and returns the new rare_tree     
+	{
+		if (rootNode.size() == 3)
+		{
+			replaceRareWords((JSONArray)rootNode.get(1) , wordCountMap);
+			replaceRareWords((JSONArray)rootNode.get(2) , wordCountMap);
+		}
+		else 
+		{
+			if (rootNode.size() == 2) 
+			{
+				String word;
+				word = rootNode.get(1).toString();
+				if (Utils.isRareWord(word , wordCountMap))
+				{
+					String tag;
+					tag = rootNode.get(0).toString();
+					rootNode.remove(1);
+					rootNode.add("_RARE_");
+				}
+			}
+		}
+	}
+
+	private static boolean isRareWord(String word, Map<String, Short> wordCountMap) 
+	{
+		if (wordCountMap.get(word) < 5)
+			return true;
+		return false;
+	}
+	
+	public static void writeNewParseTrees (List<JSONArray> parseTree)
+	{
+		String content = "";
+		for (JSONArray tree : parseTree) 
+		{
+			content += (String.format(RESULT_LINE_FORMAT, tree.toString()));
+		}
+		Utils.writeToFile ("/c:/h2p-NLP/parse_train_new.dat",content);
+		
+	}
+
+	private static void writeToFile(String path, String content) 
+	{
+		try{
+			File file = new File(path);
+			// if file doesn't exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+			
+			System.out.println("Done");
+		}
+			catch (IOException e) {
+					e.printStackTrace();
+			}
+		
+	}
+	
 }
